926bef48de16b28b0bcee455f523923f44baa475
Buff4eyes <68614831+cmokbel1@users.noreply.github.com>
2021-12-04 14:21:10 -0800

    first commit

--- index.js
+++ index.js
@@ -1,10 +1,86 @@
-// array of questions for user
-const questions = [
+// call axios
+var axios = require('axios');
+// call  markdown function
+var mD = require('./utils/generateMarkdown.js');
 
-];
+// array of questions for user
+const questions =
+  [
+    {
+      type: 'input',
+      name: 'title',
+      message: 'Project Title: '
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Project Description: '
+    },
+    {
+      type: 'input',
+      name: 'tableOfContents',
+      message: 'Table of Contents: ',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'Installation Guidelines: ',
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'How to Use: ',
+    },
+    {
+      type: 'list',
+      name: 'license',
+      message: 'Licenses:  ',
+      choices:  [`${githublink}`]
+    },
+    {
+      type: 'input',
+      name: 'contributions',
+      message: 'Contribution Guidelines: ',
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Tests Performed: ',
+    },
+    {
+      type: 'input',
+      name: 'ques',
+      message: 'Questions:  ',
+    }
+  ];
+
+// INQUIRE INPUT
+var inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+inquirer
+  .prompt(
+    // array of questions for user
+    questions
+  )
+  .then((answers) => {
+    // move answers to markdown function
+    // markdown function needs to call the write to File function
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+        prompt(error.isTtyError)
+    } else {
+      prompt(error)
+    }
+  });
+
 
 // function to write README file
-function writeToFile(fileName, data) {
+function writeToFile(filename, data) {
+  // these needs to call generate mark down
+  // then needs to push answers to proper section
+  //table of contents needs to create a list
+  // link the 'toc' to appropriate object on page
 }
 
 // function to initialize program
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,35 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  // # displays an h1 header, ## displays h2 header
+  return `# ${data.title}, 
+      
+       ## Descrition 
+        ${data.description},
 
+      ## Table of Contents
+      Installation: ${data.installation}
+      Usage: ${data.usage}
+      Contributions: ${data.contributions}
+      Licenses: ${data.licesne}
+      
+      ## Installation
+      ${data. installation},
+
+      ## Usage
+      ${data.usage},
+
+      ## Licenses
+      ${data.license}, 
+
+      ## Contributions
+      ${data.contributions}, 
+
+      ## Tests
+      ${data.tests},
+
+      ## Questions
+      ${data.ques}, 
+  
 `;
 }
 
